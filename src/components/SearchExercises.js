a79c5a39e38d97934fef0482b4fc15807d9cd991
safchik <safchik9@gmail.com>
2024-02-27 18:28:53 +0000

    scrollbar component implemented

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,9 +4,8 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
-    const [search, setSearch] = useState('')
-    const [exercises, setExercises] = useState([])
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+    const [search, setSearch] = useState('');
     const [bodyParts, setBodyParts] = useState([]);
 
     useEffect(() => {
@@ -64,7 +63,7 @@ const SearchExercises = () => {
             </Box>
 
             <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-                <HorizontalScrollbar data={bodyParts} />
+                <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
             </Box>
         </Stack>
     )
